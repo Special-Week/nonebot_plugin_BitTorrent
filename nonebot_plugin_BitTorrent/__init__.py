e3841410a2355df92231d71d6d6fcf46ff60aeab
Special-Week <2385612749@qq.com>
2023-05-24 03:24:06 +0800

    增加PluginMetadata

--- nonebot_plugin_BitTorrent/__init__.py
+++ nonebot_plugin_BitTorrent/__init__.py
@@ -1,7 +1,18 @@
 from nonebot import on_command
+from nonebot.plugin import PluginMetadata
 
 from .utils import bittorrent
 
 # 声明一个响应器, 优先级10, 向下阻断
 on_command("磁力搜索", aliases={'bt'}, priority=10, block=True,handlers=[bittorrent.main])
 
+__plugin_meta__ = PluginMetadata(
+    name="bittorrent",
+    description="磁力搜索插件",
+    usage="磁力搜索 xxx",
+    extra={
+        'author':   'Special-Week',
+        'version':  '0.0.8',
+        'priority': 10,
+    }
+)
